577c07713a702cf7c770477aa409f6317d623341
kalbirb <calvi@LAPTOP-ETMD7H4S>
2026-04-06 22:08:21 -0700

    pt2 done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 var date = new Date;
 // 2. Get the current month, day, and year
 var month = date.getMonth();
-var day = date.getDay();
+var day = date.getDate(); // Pitfall. getDay() returns day of week ie 1 = Monday, 7 = Sunday.
 var year = date.getFullYear();
 // 3. Adjust month if needed (JavaScript months are 0-based)
 month += 1;
@@ -15,8 +15,7 @@ month += 1;
 month = month.toString().padStart(2,"0");
 day = day.toString().padStart(2,"0");
 // 5. Create a string in the format: "Today is MM/DD/YYYY"
-var outputPt1 = new String;
-outputPt1 = "<h3> Today is "+ month + '/' + day + '/' + year + "</h3>";
+var outputPt1 = "<h3> Today is "+ month + '/' + day + '/' + year + "</h3>";
 // 6. Display the result in the element with id="dateOutput"
 document.getElementById("dateOutput").innerHTML = outputPt1;
 
@@ -33,20 +32,31 @@ var num2 = 21;
 //    - at least 1 decimal string (example: "19.75")
 var dec1 = 3.14;
 //    - at least 1 non-numeric string (example: "hello")
-var notString = "hello";
+var str1 = "hello";
 // TODO:
 // 2. For EACH variable:
 //
 //    a. Convert the value using Number()
-convertedNum1 = Number(num1);
+var converted1 = Number(num1);
 //    b. Check if it is NaN using Number.isNaN()
-var boolNum1 = Number.isNaN(num1);
+var bool1 = Number.isNaN(num1);
 //    c. Check if it is an integer using Number.isInteger()
-var isIntNum1 = Number.isInteger(num1);
+var isInt1 = Number.isInteger(num1);
 
-if (!notString){
-    isIntNum1 = false;
-}
+//    a. Convert the value using Number()
+var converted2 = Number(num2);
+//    b. Check if it is NaN using Number.isNaN()
+var bool2 = Number.isNaN(num2);
+//    c. Check if it is an integer using Number.isInteger()
+var isInt2 = Number.isInteger(num2);
+
+//    a. Convert the value using Number()
+var converted3 = Number(str1);
+//    b. Check if it is NaN using Number.isNaN()
+var bool3 = Number.isNaN(str1);
+//    c. Check if it is an integer using Number.isInteger()
+var isInt3 = Number.isInteger(str1);
+
 // TODO:
 // 3. For EACH value, create a sentence showing:
 //    - original value
@@ -56,11 +66,14 @@ if (!notString){
 //
 // Example format (you must create your own variables):
 // "Original: '42' -> Converted: 42 -> isNaN: false -> isInteger: true"
-var num1String = "Original: " + num1 + " -> Converted:" + convertedNum1 + " -> isNaN: " + boolNum1 + " -> isInteger: " + isIntNum1;
+var num1String = "Original: " + num1 + " -> Converted:" + converted1 + " -> isNaN: " + bool1 + " -> isInteger: " + isInt1;
+var num2String = "Original: " + num2 + " -> Converted:" + converted2 + " -> isNaN: " + bool2 + " -> isInteger: " + isInt2;
+var num3String = "Original: " + str1 + " -> Converted:" + converted3 + " -> isNaN: " + bool3 + " -> isInteger: " + isInt3;
+
 // TODO:
 // 4. Combine all your results into ONE string
 //    (you can use + to join multiple strings)
-var outputPt2 = "<h3>" + num1String + "<br>" + num1String + "</h3>";
+var outputPt2 = "<h3>" + num1String + "<br>" + num2String + "<br>" + num2String + "</h3>";
 // TODO:
 // 5. Display the final result inside the element:
 //    id="numberConversionOutput"
@@ -71,12 +84,21 @@ document.getElementById("numberConversionOutput").innerHTML = outputPt2;
 // ==========================
 
 // TODO:
+// Grade Calculator
 // 1. Create at least 2-3 numeric variables
 //
+var testMCQ;
+var testFRQ;
+var testTwo;
+var testThree;
+var gradeTotal;
 // 2. Perform calculations:
 //    - at least one addition
+gradeTotal = testMCQ + testFRQ;
 //    - at least one other operation (subtract, multiply, or divide)
 //
+gradeTotal += testTwo + testThree;
+gradeTotal /= 3; // Average Calculation based on number of Tests.
 // 3. Use at least ONE of the following:
 //    - toFixed()
 //    - toLocaleString()
